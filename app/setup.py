6410c21685b0381fcf60e847b23058128c78432d
kamenev <alexander.i.kamenev@gmail.com>
2022-10-11 20:42:05 +0300

    upd long description

--- app/setup.py
+++ app/setup.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import setuptools
 
 setuptools.setup(
     name='pg-mutex',
     version="1.0.0",
     long_description_content_type="text/markdown",
-    long_description="README.md",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     py_modules=['mutex'],
     install_requires=['psycopg2-binary'],
     python_requires='>=3.7',
